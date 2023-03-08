dbce8c5764feb4580c2c6eeeb170a6e426e27878
barbaat <barbatrejofco@gmail.com>
2023-03-08 10:39:05 +0100

    Video actualizado

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -42,7 +42,7 @@ function HeroHome() {
             {/* Modal */}
             <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
               <div className="relative pb-9/16">
-                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/802797619" title="Video" allowFullScreen></iframe>
+                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/803640776" title="Video" allowFullScreen></iframe>
               </div>
             </Modal>
 
